9a8e62525a19037c59f762b8ffed80279924414c
Thien Kim <Kim>
2026-09-17 22:13:06 -0400

    Exercice 4

--- Exercices4.cpp
+++ Exercices4.cpp
@@ -2,10 +2,18 @@
 using namespace std;
 
 int main( ){
-    int nombre;
+    int nombre, initial, inverser = 0;
 
     cout << "Entrée utilisateur : Nombre à vérifier = " << nombre << endl;
 
-    cout << " Sortie " << nombre << " n'est pas un nombre palindrome. " << endl;
-    cout << " Sortie " << nombre << " est un nombre palindrome. " << endl;
+    inital = nombre;
+
+
+    if(initial == inverser) {
+        cout << " Sortie " << initial << " est un nombre palindrome. " << endl;
+    } else {
+        cout << " Sortie " << initial << " n'est pas un nombre palindrome. " << endl;
+    }
+    
+   return 0;
 }
